9234a4c3f11d9bdaf6be3290c1f9e71f7399f490
atwolin <atwolin@gmail.com>
2024-01-06 00:17:14 +0800

    add images to main.py

--- main.py
+++ main.py
@@ -2,40 +2,69 @@ import gradio as gr
 import pandas as pd
 from algorithm import Algorithm
 from suggest import getSuggestion
+import base64
 
 muscle_groups = {
     "Leg Muscles": [
         "Quadriceps (Rectus Femoris, Vastus Lateralis, Vastus Medialis, Vastus Intermedius)",
         "Hamstrings (Biceps Femoris, Semitendinosus, Semimembranosus)",
-        "Gastrocnemius", "Soleus", "Adductor Magnus", "Tibialis Anterior",
-        "Popliteus", "Calves (Gastrocnemius and Soleus)", "Hip Adductors",
-        "Hip Abductors", "Hip Flexors", "Peroneals", "Gracilis"
+        "Gastrocnemius",
+        "Soleus",
+        "Adductor Magnus",
+        "Tibialis Anterior",
+        "Popliteus",
+        "Calves (Gastrocnemius and Soleus)",
+        "Hip Adductors",
+        "Hip Abductors",
+        "Hip Flexors",
+        "Peroneals",
+        "Gracilis",
     ],
     "Gluteal Muscles": [
-        "Gluteus Maximus", "Gluteus Medius", "Gluteus Minimus", "Tensor Fasciae Latae"
+        "Gluteus Maximus",
+        "Gluteus Medius",
+        "Gluteus Minimus",
+        "Tensor Fasciae Latae",
     ],
     "Back Muscles": [
-        "Erector Spinae", "Latissimus Dorsi", "Trapezius", "Rhomboids",
-        "Levator Scapulae", "Serratus Anterior (Upper side of ribs)", "Quadratus Lumborum"
+        "Erector Spinae",
+        "Latissimus Dorsi",
+        "Trapezius",
+        "Rhomboids",
+        "Levator Scapulae",
+        "Serratus Anterior (Upper side of ribs)",
+        "Quadratus Lumborum",
     ],
     "Abdominal Muscles": [
-        "Rectus Abdominis", "External Obliques", "Internal Obliques",
-        "Transverse Abdominis", "Internal and External Intercostals"
+        "Rectus Abdominis",
+        "External Obliques",
+        "Internal Obliques",
+        "Transverse Abdominis",
+        "Internal and External Intercostals",
     ],
     "Arm Muscles": [
-        "Biceps Brachii", "Triceps Brachii (Back of upper arm)", "Brachialis",
-        "Brachioradialis", "Forearm muscles (Flexors and Extensors)", "Anconeus"
+        "Biceps Brachii",
+        "Triceps Brachii (Back of upper arm)",
+        "Brachialis",
+        "Brachioradialis",
+        "Forearm muscles (Flexors and Extensors)",
+        "Anconeus",
     ],
     "Shoulder Muscles": [
-        "Deltoids (Anterior, Lateral, and Posterior)", "Supraspinatus",
-        "Infraspinatus", "Subscapularis", "Teres Major", "Teres Minor",
-        "Posterior Deltoid", "Pectoralis Major (Clavicular Head)",
-        "Pectoralis Major (Sternal Head)", "Pectoralis Minor"
+        "Deltoids (Anterior, Lateral, and Posterior)",
+        "Supraspinatus",
+        "Infraspinatus",
+        "Subscapularis",
+        "Teres Major",
+        "Teres Minor",
+        "Posterior Deltoid",
+        "Pectoralis Major (Clavicular Head)",
+        "Pectoralis Major (Sternal Head)",
+        "Pectoralis Minor",
     ],
-    "Chest Muscles": [
-        "Pectoralis Major", "Pectoralis Minor"
-    ]
+    "Chest Muscles": ["Pectoralis Major", "Pectoralis Minor"],
 }
+
 
 def get_workout_plan(*args):
     # The last argument is tolerance, and the rest are selected muscle groups
@@ -44,44 +73,52 @@ def get_workout_plan(*args):
 
     # Load equipment data
     df = pd.read_csv("data/equipment.csv")
-    
+
     # User input is already a list of selected muscle groups
     userInput = [selected_muscles, int(tolerance)]
     algorithm = Algorithm(userInput)
-    
+
     # Run the algorithm and get the result
     result, cost = algorithm.method()
-    
+
     # Handle the output of the algorithm
     if not result:
         return "No suitable workout plan found."
-    
-    output_string=""
+
+    output_string = ""
     # output_string += "Suggested workout plan for: " + ', '.join(selected_muscles) + "\n"
     # output_string += "Total cost: " + str(cost) + " minutes\n\n"
     output_string += "Workout Plan:\n\n"
+    image_html = ""
     for equipment, wait_time in result:
         output_string += f"{equipment} - wait for {wait_time} minutes\n"
         combined, totalTime = getSuggestion([equipment])
-        output_string += f'      around {totalTime} minutes workouts\n'
+        output_string += f"      around {totalTime} minutes workouts\n"
         for exercise in combined:
-            output_string += '        - ' + exercise
-        output_string += '\n'
-    
-    return output_string
+            output_string += "        - " + exercise
+        output_string += "\n"
+        # Read the local image file
+        image_name = equipment.replace("\\", "_")
+        # print(image_name)
+        image_html += f"<img src='file/images/{image_name}.webp' style='max-width:300px; max-height:300px;'>"
+
+    return [output_string, image_html]
 
 
 # Create Gradio interface without collapsible sections
-inputs = [gr.CheckboxGroup(choices=choices, label=label) for label, choices in muscle_groups.items()]
+inputs = [
+    gr.CheckboxGroup(choices=choices, label=label)
+    for label, choices in muscle_groups.items()
+]
 inputs.append(gr.Dropdown(choices=[0, 10, 20, 30], label="Tolerance (minutes)"))
 
 interface = gr.Interface(
     fn=get_workout_plan,
     inputs=inputs,
-    outputs="text",
+    outputs=["text", "html"],
     title="Workout Optimizer",
-    description="Select muscle groups from each category and your wait tolerance to get a workout plan."
+    description="Select muscle groups from each category and your wait tolerance to get a workout plan.",
 )
 
 if __name__ == "__main__":
-    interface.launch()
+    interface.launch(allowed_paths=["."])
--- suggest.py
+++ suggest.py
@@ -2,9 +2,10 @@ import pandas as pd
 import numpy as np
 import json
 from math import floor, ceil
+
 # from algorithm import Algorithm
 
-FILENAME = 'data\workout_type.csv'
+FILENAME = "data/workout_type.csv"
 EASY = 4
 MEDIUM = 3
 HARD = 2
@@ -12,7 +13,7 @@ HARD = 2
 # allUnitList = []
 
 # def jsonToCsv(filePath):
-  
+
 #   # load json
 #   with open(filePath, 'r') as fl:
 #     data = json.load(fl)
@@ -25,7 +26,7 @@ HARD = 2
 #     for exercise in unit['exercises']:
 #       unitExer.append(exercise['exer_name'])
 #       unitDes.append(exercise['description'])
-    
+
 #     allUnitList.append([unitName, unitExer, unitDes])
 #   print(len(allUnitList))
 
@@ -35,169 +36,209 @@ HARD = 2
 #   df_data.to_csv('data\workout_type.csv', index=False)
 
 # # jsonToCsv('data\workout_type.json')
-  
+
 ##########################################################################
-  
+
+
 def getExerSuggestion(equipments, filePath=FILENAME):
-  # dummy as index
-  df_dummy = pd.read_csv(filePath)['unitName'].values.tolist()
-  df_dummy = [x.lower() for x in df_dummy]
+    # dummy as index
+    df_dummy = pd.read_csv(filePath)["unitName"].values.tolist()
+    df_dummy = [x.lower() for x in df_dummy]
 
-  # read file
-  df_exer = pd.read_csv(filePath)
-  columns = df_exer.columns
+    # read file
+    df_exer = pd.read_csv(filePath)
+    columns = df_exer.columns
 
-  allEquipExer = {}
-  topOutput = []
-  easyOutput = []
-  mediumOutput = []
-  hardOutput = []
+    allEquipExer = {}
+    topOutput = []
+    easyOutput = []
+    mediumOutput = []
+    hardOutput = []
 
-  # iterate over all equipments 
-  for equipment in equipments:
-    idx = df_dummy.index(equipment)
-    entry = df_exer.iloc[idx]
+    # iterate over all equipments
+    for equipment in equipments:
+        idx = df_dummy.index(equipment)
+        entry = df_exer.iloc[idx]
 
-    (topExer, hardExer, mediumExer, easyExer,
-     topDes, hardDes, mediumDes, easyDes,
-     topPro, hardPro, mediumPro, easyPro) = getExer(entry[columns[1]].split(', '), 
-                                            entry[columns[2]].split(', '), 
-                                            entry[columns[3]].split(', '), 
-                                            entry[columns[4]].split(', '), 
-                                            entry[columns[6]].split(', '), 
-                                            n=3,
-                                            condition=True)
-    
-    if len(topExer):
-      topOutput = suggestExerCount(topExer, topPro)
-    if len(easyExer):
-      easyOutput = suggestExerCount(easyExer, easyPro, EASY)
-    if len(mediumExer):
-      mediumOutput = suggestExerCount(mediumExer, mediumPro, MEDIUM)
-    if len(hardExer):
-      hardOutput = suggestExerCount(hardExer, hardPro, HARD)
+        (
+            topExer,
+            hardExer,
+            mediumExer,
+            easyExer,
+            topDes,
+            hardDes,
+            mediumDes,
+            easyDes,
+            topPro,
+            hardPro,
+            mediumPro,
+            easyPro,
+        ) = getExer(
+            entry[columns[1]].split(", "),
+            entry[columns[2]].split(", "),
+            entry[columns[3]].split(", "),
+            entry[columns[4]].split(", "),
+            entry[columns[6]].split(", "),
+            n=3,
+            condition=True,
+        )
 
-    allEquipExer[equipment] = { 'topExer': [topExer, topDes, topOutput],
-                                'hardExer': [hardExer, hardDes, hardOutput], 
-                                'mediumExer': [mediumExer, mediumDes, mediumOutput], 
-                                'easyExer': [easyExer, easyDes, easyOutput]}
-    
-  return allEquipExer
+        if len(topExer):
+            topOutput = suggestExerCount(topExer, topPro)
+        if len(easyExer):
+            easyOutput = suggestExerCount(easyExer, easyPro, EASY)
+        if len(mediumExer):
+            mediumOutput = suggestExerCount(mediumExer, mediumPro, MEDIUM)
+        if len(hardExer):
+            hardOutput = suggestExerCount(hardExer, hardPro, HARD)
 
-def getExer(exerList, exerDesc, exerDiffList, 
-            exerCount, exerTime, n=3, condition=False):
-  topExer = []
-  hardExer = []
-  mediumExer = []
-  easyExer = []
+        allEquipExer[equipment] = {
+            "topExer": [topExer, topDes, topOutput],
+            "hardExer": [hardExer, hardDes, hardOutput],
+            "mediumExer": [mediumExer, mediumDes, mediumOutput],
+            "easyExer": [easyExer, easyDes, easyOutput],
+        }
 
-  topDes = []
-  hardDes = []
-  mediumDes = []
-  easyDes = []
+    return allEquipExer
 
-  topPro = [[], []]
-  hardPro = [[], []]
-  mediumPro = [[], []]
-  easyPro = [[], []]
 
-  for i in range(n):
-    exerDescription = exerDesc[i]
-    exerCount_ = exerCount[i]
-    exerTime_ = exerTime[i]
+def getExer(
+    exerList, exerDesc, exerDiffList, exerCount, exerTime, n=3, condition=False
+):
+    topExer = []
+    hardExer = []
+    mediumExer = []
+    easyExer = []
 
-  # return any top-n exercises
-    if(not condition):
-       topExer.append(exerList[i])
-       topDes.append(exerDescription)
-       topPro[0].append(exerCount_)
-       topPro[1].append(exerTime_)
+    topDes = []
+    hardDes = []
+    mediumDes = []
+    easyDes = []
 
-  # return top-n exercise based on rules
-    else:
-      if int(exerDiffList[i]) < 2:
-        easyExer.append(exerList[i])
-        easyDes.append(exerDescription)
-        easyPro[0].append(exerCount_)
-        easyPro[1].append(exerTime_)
-      elif int(exerDiffList[i]) < 4:
-        mediumExer.append(exerList[i])
-        mediumDes.append(exerDescription)
-        mediumPro[0].append(exerCount_)
-        mediumPro[1].append(exerTime_)
-      else:
-        hardExer.append(exerList[i])
-        hardDes.append(exerDescription)
-        hardPro[0].append(exerCount_)
-        hardPro[1].append(exerTime_)
+    topPro = [[], []]
+    hardPro = [[], []]
+    mediumPro = [[], []]
+    easyPro = [[], []]
 
-  return (topExer, hardExer, mediumExer, easyExer,
-          topDes, hardDes, mediumDes, easyDes,
-          topPro, hardPro, mediumPro, easyPro)
+    for i in range(n):
+        exerDescription = exerDesc[i]
+        exerCount_ = exerCount[i]
+        exerTime_ = exerTime[i]
+
+        # return any top-n exercises
+        if not condition:
+            topExer.append(exerList[i])
+            topDes.append(exerDescription)
+            topPro[0].append(exerCount_)
+            topPro[1].append(exerTime_)
+
+        # return top-n exercise based on rules
+        else:
+            if int(exerDiffList[i]) < 2:
+                easyExer.append(exerList[i])
+                easyDes.append(exerDescription)
+                easyPro[0].append(exerCount_)
+                easyPro[1].append(exerTime_)
+            elif int(exerDiffList[i]) < 4:
+                mediumExer.append(exerList[i])
+                mediumDes.append(exerDescription)
+                mediumPro[0].append(exerCount_)
+                mediumPro[1].append(exerTime_)
+            else:
+                hardExer.append(exerList[i])
+                hardDes.append(exerDescription)
+                hardPro[0].append(exerCount_)
+                hardPro[1].append(exerTime_)
+
+    return (
+        topExer,
+        hardExer,
+        mediumExer,
+        easyExer,
+        topDes,
+        hardDes,
+        mediumDes,
+        easyDes,
+        topPro,
+        hardPro,
+        mediumPro,
+        easyPro,
+    )
+
 
 def suggestExerCount(exers, properties, minute=EASY):
-  """
+    """
     exers: exercise list
     properties: [[Count], [secondPerCount]]
     minute: exercise length (how long to do the exercise)
-  """
+    """
 
-  # count how long to do 1 exercise
-  exerCount = len(exers)
-  minutePerExer = floor(minute / exerCount)
+    # count how long to do 1 exercise
+    exerCount = len(exers)
+    minutePerExer = floor(minute / exerCount)
 
-  output = []
+    output = []
 
-  for i in range(exerCount):
-    count = floor(minutePerExer*60 / int(properties[1][i]))
-    sets = count / int(properties[0][i])
-    setStr = f'{int(sets)}' if sets <= floor(sets) else f'{floor(sets)} - {ceil(sets)}'
-    isPlural = 'sets' if sets > 1 else 'set'
+    for i in range(exerCount):
+        count = floor(minutePerExer * 60 / int(properties[1][i]))
+        sets = count / int(properties[0][i])
+        setStr = (
+            f"{int(sets)}" if sets <= floor(sets) else f"{floor(sets)} - {ceil(sets)}"
+        )
+        isPlural = "sets" if sets > 1 else "set"
 
-    output.append(f'=> {int(properties[0][i])} times for {setStr} {isPlural}')
+        output.append(f"=> {int(properties[0][i])} times for {setStr} {isPlural}")
 
-  return output
+    return output
+
 
 def getSuggestion(equipmentList):
-  allEquipExer = getExerSuggestion(equipmentList)
+    allEquipExer = getExerSuggestion(equipmentList)
 
-  for equipment in allEquipExer:
-    equipExer = allEquipExer[equipment]
+    for equipment in allEquipExer:
+        equipExer = allEquipExer[equipment]
 
-    totalTime = 0
-    top = []
-    easy = []
-    medium = []
-    hard = []
+        totalTime = 0
+        top = []
+        easy = []
+        medium = []
+        hard = []
 
-    if len(equipExer["topExer"][0]):
-      pass
-    
-    if len(equipExer["easyExer"][0]):
-      totalTime += EASY*len(equipExer["easyExer"][0])
-      for i in range(len(equipExer["easyExer"][0])):
-        easy.append(f'{equipExer["easyExer"][0][i]} {equipExer["easyExer"][2][i]}\n')
+        if len(equipExer["topExer"][0]):
+            pass
 
-    if len(equipExer["mediumExer"][0]):
-      totalTime += MEDIUM*len(equipExer["mediumExer"][0])
-      for i in range(len(equipExer["mediumExer"][0])):
-        medium.append(f'{equipExer["mediumExer"][0][i]} {equipExer["mediumExer"][2][i]}\n')
+        if len(equipExer["easyExer"][0]):
+            totalTime += EASY * len(equipExer["easyExer"][0])
+            for i in range(len(equipExer["easyExer"][0])):
+                easy.append(
+                    f'{equipExer["easyExer"][0][i]} {equipExer["easyExer"][2][i]}\n'
+                )
 
-    if len(equipExer["hardExer"][0]):
-      totalTime += HARD*len(equipExer["hardExer"][0])
-      for i in range(len(equipExer["hardExer"][0])):
-        hard.append(f'{equipExer["hardExer"][0][i]} {equipExer["hardExer"][2][i]}\n')
+        if len(equipExer["mediumExer"][0]):
+            totalTime += MEDIUM * len(equipExer["mediumExer"][0])
+            for i in range(len(equipExer["mediumExer"][0])):
+                medium.append(
+                    f'{equipExer["mediumExer"][0][i]} {equipExer["mediumExer"][2][i]}\n'
+                )
 
-    combined = easy + medium + hard
+        if len(equipExer["hardExer"][0]):
+            totalTime += HARD * len(equipExer["hardExer"][0])
+            for i in range(len(equipExer["hardExer"][0])):
+                hard.append(
+                    f'{equipExer["hardExer"][0][i]} {equipExer["hardExer"][2][i]}\n'
+                )
 
-    # print(f'{equipment} for <{totalTime} minutes')
-    # # print(f'topExer: {", ".join(equipExer["topExer"])}')
-    # print(f'easy: {", ".join(easy)}')
-    # print(f'medium: {", ".join(medium)}')
-    # print(f'hard: {", ".join(hard)}')
-    # print()
+        combined = easy + medium + hard
 
-    return combined, totalTime
+        # print(f'{equipment} for <{totalTime} minutes')
+        # # print(f'topExer: {", ".join(equipExer["topExer"])}')
+        # print(f'easy: {", ".join(easy)}')
+        # print(f'medium: {", ".join(medium)}')
+        # print(f'hard: {", ".join(hard)}')
+        # print()
+
+        return combined, totalTime
+
 
 # equipmentList = ['Leg Press and Hack Squat Machine'.lower()]
 # combined, totalTime = getSuggestion(equipmentList)
